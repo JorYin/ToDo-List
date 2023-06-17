424ef65f9be360d78f1a323ce214e70bae3a03a2
JorYin <134739719+JorYin@users.noreply.github.com>
2023-06-17 18:08:53 -0500

    Updating ToDo-List folder

---
+++ main.js
@@ -0,0 +1,53 @@
+
+//HTML Selectors
+const addToDoButton = document.getElementById("addToDo");
+const toDoList = document.getElementById("ulList");
+const toDoInput = document.getElementById("inputField");
+
+//Event Listeners
+addToDoButton.addEventListener("click", addTask);
+toDoList.addEventListener("click", taskFinished);
+data();
+
+//Funcations
+function addTask(event){
+
+  event.preventDefault();
+
+  if(toDoInput.value.trim() === ""){
+    alert("Please write something in the box!")
+  }else{
+    const list = document.createElement("li");
+    const listSpan = document.createElement("span");
+    list.innerHTML = toDoInput.value;
+    listSpan.innerHTML = "X";
+    list.appendChild(listSpan);
+    toDoList.appendChild(list);
+    save();
+  }
+    toDoInput.value = "";
+
+}
+
+function taskFinished(event){
+
+  if(event.target.tagName === "LI"){
+    event.target.classList.toggle("finished");
+    save();
+  }else if(event.target.tagName === "SPAN"){
+    event.target.parentElement.remove();
+    save();
+  }
+
+}
+
+function save(){
+  localStorage.setItem("data", toDoList.innerHTML);
+}
+
+function data(){
+  toDoList.innerHTML = localStorage.getItem("data");
+}
+
+
+
