e7eb7ca98a5cd009b9be06c749b85f59bd1cac66
JorYin <134739719+JorYin@users.noreply.github.com>
2023-06-29 16:58:33 -0500

    Comments and Phone view

--- src/main.js
+++ src/main.js
@@ -13,10 +13,14 @@ data();
 function addTask(event){
 
   event.preventDefault();
-
+  //If there isn't any text in the input box alert the user!
   if(toDoInput.value.trim() === ""){
     alert("Please write something in the box!")
   }else{
+    //If there is text then create a list element and a span element
+    //Then with the li element put the value into the HTML
+    //Make the span the letter x
+    //Then add both children nodes into the main node which is the HTML element
     const list = document.createElement("li");
     const listSpan = document.createElement("span");
     list.innerHTML = toDoInput.value;
@@ -25,12 +29,14 @@ function addTask(event){
     toDoList.appendChild(list);
     save();
   }
+    //clear input
     toDoInput.value = "";
 
 }
 
 function taskFinished(event){
-
+  //If LI is clicked toggle task as finished
+  //If SPAN clicked remove the node from the parent element
   if(event.target.tagName === "LI"){
     event.target.classList.toggle("finished");
     save();
@@ -41,11 +47,13 @@ function taskFinished(event){
 
 }
 
-const save = () => {
+
+//saving data on local web browser
+function save () {
   localStorage.setItem("data", toDoList.innerHTML);
 }
 
-const data = () => {
+function data () {
   toDoList.innerHTML = localStorage.getItem("data");
 }
 
